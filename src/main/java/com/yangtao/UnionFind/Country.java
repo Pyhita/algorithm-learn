0bb7a19f30a74777a8a8256bb2d5c4b8f768fd77
Pyhita <pyhita.dev@gmail.com>
2022-03-10 20:35:28 +0800

    并查集实现

---
+++ src/main/java/com/yangtao/UnionFind/Country.java
@@ -0,0 +1,10 @@
+package com.yangtao.UnionFind;
+
+/**
+ * @Author: pyhita
+ * @Date: 2022/3/10
+ * @Descrption: com.yangtao.UnionFind
+ * @Version: 1.0
+ */
+public class Country {
+}
---
+++ src/main/java/com/yangtao/UnionFind/Student.java
@@ -0,0 +1,16 @@
+package com.yangtao.UnionFind;
+
+/**
+ * @Author: pyhita
+ * @Date: 2022/3/10
+ * @Descrption: com.yangtao.UnionFind
+ * @Version: 1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
